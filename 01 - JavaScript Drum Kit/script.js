a4dcc258dbbbe9f134a36aaedf603220a152b9a0
kiran shrinivaas <hayabuzakiran@gmail.com>
2022-11-16 18:19:00 +0530

    js updated

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,20 +1,45 @@
-window.addEventListener('keydown',playsound);
-
-function playsound(e)
+function audiosound(e)
 {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    // changeaudioheader(key);
     if(!audio) return;
     audio.currentTime=0;
     audio.play()
     key.classList.add('playing');
+    const span = key.querySelector('.sound');
+    const p = document.querySelector('.audio-name');
+    p.textContent = span.textContent.toUpperCase(); 
 };
 
 
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+function changeaudioheader(e){
+    console.log(e.currentTarget,e.target);
+    console.dir(e.currentTarget);
+    
+    const key = e.currentTarget;
+    const audio = document.querySelector(`audio[data-key="${key.dataset.key}"]`);
+    if(!audio) return;
+    audio.currentTime=0;
+    audio.play();
+    const kbd = key.querySelector('kbd');
+    const dataKey = kbd.textContent;
+    const span = key.querySelector('.sound');
+    const p = document.querySelector('.audio-name');
+    p.textContent = span.textContent.toUpperCase();
+};
 
 function removeTransition(e){
     this.classList.remove('playing');
-}
+};
+
+const keys = document.querySelectorAll('.key');
+console.log(keys);
+
+window.addEventListener('keydown',audiosound);
+keys.forEach((key)=>{
+    console.log(key);
+    key.addEventListener('click',changeaudioheader);   
+    key.addEventListener('transitionend',removeTransition);
+    
+});
