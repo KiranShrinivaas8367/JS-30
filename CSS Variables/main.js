9d32c169ae4ff795b3c57b610a4c300dbc7e66ac
kiran shrinivaas <hayabuzakiran@gmail.com>
2022-11-23 18:46:00 +0530

    Styles->added

--- CSS Variables/main.js
+++ CSS Variables/main.js
@@ -1,10 +1,16 @@
 const inputs = document.querySelectorAll('.controls input');
 
-inputs.forEach(input => input.addEventListener('change',handleUpdate));
-inputs.forEach(input => input.addEventListener('mousemove',handleUpdate));
+inputs.forEach(input => input.addEventListener('change',Update));
+inputs.forEach(input => input.addEventListener('mousemove',Update));
 
 
-function handleUpdate(){
+function Update(){
     const suffix = this.dataset.sizing;
     document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
 }
+
+/*<-----Debugging Purpose ------------>*/
+     //console.log(this.dataset)
+    // console.log(suffix, this.name, this.value)
+    // this.dataset.value = `${this.value}${suffix}`;
+    // console.log(this.value,suffix);
