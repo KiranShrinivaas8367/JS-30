bff2dbe7c32007a6eacad6589b6748f2e9c9d346
kiran shrinivaas <hayabuzakiran@gmail.com>
2022-11-26 11:29:48 +0530

    toggleopen->functionality added

---
+++ Flex Panel Gallery/script.js
@@ -0,0 +1,27 @@
+const panels = document.querySelectorAll('.panel');
+
+// function toggleOpen()
+// {
+//     // console.log('hello'); debugging purpose
+//     this.classList.toggle('open');
+    
+// }
+
+function toggleopenActive(e){
+    // console.log(e.propertyName); debugging purpose
+    // if(e.propertyName.includes('flex'))
+    // {
+    //     this.classList.toggle('open-active');
+    // }
+    
+    this.classList.toggle('open');
+    
+    setTimeout(()=>
+    {
+        this.classList.toggle('open-active');
+    },500);
+}
+
+panels.forEach(panel => panel.addEventListener('mouseenter',toggleopenActive));
+panels.forEach(panel => panel.addEventListener('mouseleave',toggleopenActive));
+panels.forEach(panel => panel.addEventListener('transitionend',toggleopenActive))
