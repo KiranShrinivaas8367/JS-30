d07a64ad8867df674701a5a24daeb24d918b12e9
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-08 17:20:24 +0530

    navheight-functionality->added

--- Sticky Nav/main.js
+++ Sticky Nav/main.js
@@ -1,8 +1,20 @@
 const nav = document.querySelector('#main')
+navtop=nav.offsetTop;
 
 function navScroll(){
-    navtop=nav.offsetTop;
-    console.log(navtop,scrollY);
+    
+    // console.log(navtop,scrollY);
+    if(window.scrollY >= navtop){
+        document.body.style.paddingTop = nav.offsetHeight + 'px'
+        // console.log(nav.offsetHeight + 'px');
+        document.body.classList.add('fixed-nav')
+    }
+    else{
+        document.body.classList.remove('fixed-nav')
+        document.body.style.paddingTop = 0
+        
+    }
+
 }
 
-window.addEventListener('scroll',navScroll)
+window.addEventListener('scroll',navScroll);
