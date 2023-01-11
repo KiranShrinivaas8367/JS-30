d4818b380b9b62bf533cda99080fd6493408a6a3
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-11 23:45:59 +0530

    width,height

--- Follow Along Link Highlighter/main.js
+++ Follow Along Link Highlighter/main.js
@@ -6,6 +6,8 @@ document.body.append(highlight)
 function highlightlink(){
     const linkcoords = this.getBoundingClientRect()
     console.log(linkcoords)
+    highlight.style.width=`${linkcoords.width}px`;
+    highlight.style.height=`${linkcoords.height}px`
 }
 
 links.forEach(a=>a.addEventListener('mouseenter',highlightlink))
