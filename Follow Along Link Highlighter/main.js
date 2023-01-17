0f49b549c54ccc30d23495e48a2933ccec030375
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-17 21:37:07 +0530

    transform-fixed

--- Follow Along Link Highlighter/main.js
+++ Follow Along Link Highlighter/main.js
@@ -8,7 +8,7 @@ function highlightlink(){
     console.log(linkcoords)
     highlight.style.width =`${linkcoords.width}px`;
     highlight.style.height =`${linkcoords.height}px`;
-    highlight.style.transform =`translate(400px,400px)`;
+    highlight.style.transform =`translate(${linkcoords.left}px,${linkcoords.top}px)`;
 }
 
 links.forEach(a=>a.addEventListener('mouseenter',highlightlink))
