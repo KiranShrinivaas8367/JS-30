2857119d3f1f0d9bfcdb7892c7ec024ad445d7c7
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-17 23:16:53 +0530

    coords->added

--- Follow Along Link Highlighter/main.js
+++ Follow Along Link Highlighter/main.js
@@ -6,9 +6,13 @@ document.body.append(highlight)
 function highlightlink(){
     const linkcoords = this.getBoundingClientRect()
     console.log(linkcoords)
+    const coords = {
+        top: linkcoords.top + window.scrollY,
+        left: linkcoords.left + window.scrollX
+    };
     highlight.style.width =`${linkcoords.width}px`;
     highlight.style.height =`${linkcoords.height}px`;
-    highlight.style.transform =`translate(${linkcoords.left}px,${linkcoords.top}px)`;
+    highlight.style.transform =`translate(${coords.left}px,${coords.top}px)`;
 }
 
 links.forEach(a=>a.addEventListener('mouseenter',highlightlink))
