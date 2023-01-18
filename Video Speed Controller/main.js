b4ec564379a1161d5d8f1cefe1411c270b184d55
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-18 21:01:21 +0530

    percentage->founded

--- Video Speed Controller/main.js
+++ Video Speed Controller/main.js
@@ -4,7 +4,9 @@ const video = document.querySelector('.flex')
 
 speed.addEventListener('mousemove',function(e){
     // console.log(e.pageY,this.offsetTop)
-    console.log(this.offsetHeight)
+    // console.log(this.offsetHeight)
     const y = e.pageY - this.offsetTop;
-    console.log(`${y}px`);
+    // console.log(`${y}px`);
+    const percentage = y/this.offsetHeight;
+    
 });
