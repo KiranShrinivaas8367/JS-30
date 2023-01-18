04fd2fd37c90fdfe95ab80b31a0526f1bb608628
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-18 21:26:00 +0530

    playbackrate->added

--- Video Speed Controller/main.js
+++ Video Speed Controller/main.js
@@ -5,9 +5,16 @@ const video = document.querySelector('.flex')
 speed.addEventListener('mousemove',function(e){
     // console.log(e.pageY,this.offsetTop)
     // console.log(this.offsetHeight)
-    const y = e.pageY - this.offsetTop;
     // console.log(`${y}px`);
+    const y = e.pageY - this.offsetTop;
     const percentage = y/this.offsetHeight;
-    const height = Math.ceil(percentage*100);
-    console.log(height + '%')
+    const height = Math.ceil(percentage*100) + '%';
+    // console.log(height)
+    const min = 0.2;
+    const max = 6;
+    bar.style.height = height;
+    const playbackrate = percentage * (max-min) + min;
+    // console.log(playbackrate)
+    bar.textContent = playbackrate.toFixed(1)+'x';
+    video.playbackRate = playbackrate;
 });
