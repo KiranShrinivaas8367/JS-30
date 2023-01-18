0582d42e6d2c19044c850b2b139a671419650fb1
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-18 20:56:08 +0530

    offsetHeight->added

--- Video Speed Controller/main.js
+++ Video Speed Controller/main.js
@@ -2,6 +2,9 @@ const speed = document.querySelector('.speed')
 const bar = document.querySelector('.speed-bar')
 const video = document.querySelector('.flex')
 
-speed.addEventListener('mousemove',(e)=>{
-    console.log(e);
-})
+speed.addEventListener('mousemove',function(e){
+    // console.log(e.pageY,this.offsetTop)
+    console.log(this.offsetHeight)
+    const y = e.pageY - this.offsetTop;
+    console.log(`${y}px`);
+});
