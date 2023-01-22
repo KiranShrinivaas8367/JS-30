0ca8c283bb60041cac60f41c11939a868b20a71d
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-22 23:52:21 +0530

    offset

--- Mouse Move Shadow/main.js
+++ Mouse Move Shadow/main.js
@@ -2,7 +2,12 @@ const hero = document.querySelector('.hero')
 const tag = document.querySelector('h1')
 
 function shadow(e){
-    console.log(e);
+    // const width = hero.offsetWidth;
+    // const height = hero.offsetHeight;
+    const{offsetWidth : width, offsetHeight: height} = hero;
+    const{offsetX : x, offsetY: y} = e;   
+    // console.log(x,y)  
+    console.log(e.target.className)
 }
 
 hero.addEventListener('mousemove',shadow);
