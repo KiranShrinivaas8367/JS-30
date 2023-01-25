e244ea7d0c6c0b02f882069d098029c5eb43bd77
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-26 00:30:11 +0530

    function->completed

--- Sort Without Articles/main.js
+++ Sort Without Articles/main.js
@@ -1,1 +1,16 @@
-const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled',
+ 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function skip(name){
+    return name.replace(/^(a |the |an )/i,'').trim();
+}
+
+ const sorted = bands.sort(function(a,b){
+     if(skip(a)>skip(b))
+     return 1;
+     else 
+     return -1;
+ });
+
+ document.querySelector('#bands').innerHTML=sorted.map(band => `<li>${band}</li>`).join(' ');
+ 
